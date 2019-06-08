cabe7ba79fc25ddba0fafb62316a3ad86ed754e9
jhon ever <jhoneverguzman@gmail.com>
2019-06-08 16:14:07 -0500

    entrega 2

---
+++ salajuegos/Modulo-Administrador.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to play gamer </title>
+	<meta charset="utf-8">
+
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body class="menu__item">
+	<div id="login">
+		<div align="center">
+		<img src="css/fondo.jpg">
+
+		<h1>welcome the play gamer to Capachos  </h1>
+        
+    <form action="administracion.php">
+       <div class="main-container">
+      
+        <ul class="menu__item">
+          <li class=" "><a href="administracion.php" class=" ">Módulo Administracion</a>
+          </li><br>
+          </br>
+          <ul class="menu__item">
+          <li class=""><a href="campeonato.php" class=" ">Módulo Campeonato</a></li><br>
+        <ul class="menu__item">
+          <li class=" "><a href="consolas.php"class="" >Módulo Mantenimiento y soporte de Consolas</a></li>
+               <br>
+               </br>
+          
+        <ul class="menu__item">
+          <li class=""><a href="facturacion.php" class=" ">Módulo Facturacion</a></li><br>
+               </br>
+        
+           </div>
+        </ul>
+     
+
+       </body>
+</html>
---
+++ salajuegos/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to play gamer </title>
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	
+</head>
+<body>
+	<div id="login">
+      <div align="center">
+      <img src="css/fondo.jpg">
+      <h1>welcome the play gamer to Capachos  </h1>
+        </div>
+         <form action= "Modulo-Administrador.php" method="GET">
+            <label>Usuario: </label>
+            <input type="text" name="user" required=" Usuario" /><br>
+            <label>Contraseña: </label>
+            <input type="password" name="password"  required="password" /><br><br>
+            <input type="submit" value="Registrar"/>
+         </form>
+      </div>
+</body>
+</html>
